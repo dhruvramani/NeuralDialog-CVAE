095bcea07ba33bf4358ada103427fe4bb277b5ba
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-06 14:19:11 +0530

    debug

--- data_apis/data_utils.py
+++ data_apis/data_utils.py
@@ -60,8 +60,8 @@ class LongDataLoader(object):
                 cut_start = range(0, num_seg*self.step_size, step_size)
                 cut_end = range(self.backward_size, num_seg*self.step_size+self.backward_size, step_size)
                 assert cut_end[-1] < max_len
-                cut_start = [0] * (self.backward_size-2) +cut_start # since we give up on the seq training idea
-                cut_end = range(2, self.backward_size) + cut_end
+                cut_start = [0] * (self.backward_size-2) + list(cut_start) # since we give up on the seq training idea
+                cut_end = range(2, self.backward_size) + list(cut_end)
             else:
                 cut_start = [0] * (max_len-2)
                 cut_end = range(2, max_len)
