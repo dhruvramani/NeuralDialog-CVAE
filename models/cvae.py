2b78c393806b42475a6e8b00917baf2953d0038b
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-06 16:39:46 +0530

    changed metrics

--- models/cvae.py
+++ models/cvae.py
@@ -540,6 +540,7 @@ class KgRnnCVAE(BaseTFModel):
                     local_tokens.append(pred_tokens)
 
                 max_bleu, avg_bleu = utils.get_bleu_stats(true_tokens, local_tokens)
+                print(true_tokens.shape, local_tokens.shape)
                 f1 = f1_score(true_tokens, local_tokens, average='micro')
                 f1_scores.append(f1)
                 recall_bleus.append(max_bleu)
@@ -549,7 +550,7 @@ class KgRnnCVAE(BaseTFModel):
 
         avg_recall_bleu = float(np.mean(recall_bleus))
         avg_prec_bleu = float(np.mean(prec_bleus))
-        avg_f1 = float(np.mean(f1_scores))#2*(avg_prec_bleu*avg_recall_bleu) / (avg_prec_bleu+avg_recall_bleu+10e-12)
+        avg_f1 = float(np.mean(f1_scores)) #2*(avg_prec_bleu*avg_recall_bleu) / (avg_prec_bleu+avg_recall_bleu+10e-12)
         report = "Avg recall BLEU %f, avg precision BLEU %f and F1 %f (only 1 reference response. Not final result)" \
                  % (avg_recall_bleu, avg_prec_bleu, avg_f1)
         print(report)
