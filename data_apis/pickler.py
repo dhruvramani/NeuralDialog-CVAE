e73327ab9c0c7e6f932355f7ab9c1ea442f080c3
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-06 14:46:19 +0530

    done

--- data_apis/pickler.py
+++ data_apis/pickler.py
@@ -29,7 +29,7 @@ with open(_DIR + "data/commonsense/storyid_partition.txt", "r") as f:
             charA, charB = characters[dialog["A"]], characters[dialog["B"]]
             if(charA["app"] == True):
                 try :
-                    mystring = str(charA["emotion"]["ann0"]["text"][0])
+                    mystring = str(charA["emotion"]["ann0"]["plutchik"][0])
                     re.sub('[^A-Za-z]+', '', mystring)
                     uttr = ("A", linei["text"], [mystring])
                     utterances.append(uttr)
@@ -37,7 +37,7 @@ with open(_DIR + "data/commonsense/storyid_partition.txt", "r") as f:
                     pass
             if(charB["app"] == True):
                 try :
-                    mystring = str(charB["emotion"]["ann0"]["text"][0])
+                    mystring = str(charB["emotion"]["ann0"]["plutchik"][0])
                     re.sub('[^A-Za-z]+', '', mystring)
                     uttr = ("B", linei["text"], [mystring])
                     utterances.append(uttr)
