effadb07e5241e1ba04759804bbe5210289a84cf
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-02-07 12:46:20 +0530

    pickler changed

--- data_apis/pickler.py
+++ data_apis/pickler.py
@@ -9,6 +9,26 @@ file = open(_DIR + "data/commonsense/json_version/annotations.json", "r")
 data = json.load(file)
 to_write = {"train" : list(), "valid" : list(), "test" : list()}
 count = 0
+classes = ["joy", "trust", "fear", "surprise", "sadness", "disgust", "anger", "anticipation"]
+
+def get_labels(charay):
+    try :
+        ann = [charay["emotion"]["ann0"]["plutchik"], charay["emotion"]["ann1"]["plutchik"], charay["emotion"]["ann2"]["plutchik"]]
+        final_dict = dict()
+        for classi in classes:
+            final_dict[classi] = [1, 1, 1]
+
+        for idx in range(len(ann)):
+            for i in ann[idx]:
+                if(i[:-2] in final_dict.keys()):
+                    final_dict[i[:-2]][idx] = int(i[-1])
+
+        majority = [key for key in final_dict.keys() if floor(sum(final_dict[key]) / 3) >= 2]
+        onehot = [1 for i in classes if i in majority else 0]
+        return onehot
+        #re.sub('[^A-Za-z]+', '', mystring)
+    except :
+        return None
 
 with open(_DIR + "data/commonsense/storyid_partition.txt", "r") as f:
     for line in f:
@@ -21,7 +41,7 @@ with open(_DIR + "data/commonsense/storyid_partition.txt", "r") as f:
             continue
         dialog["A"] = chars[0]
         dialog["B"] = chars[1] # TODO : Change
-        dialog["topic"] = story["title"].lower()
+        #dialog["topic"] = story["title"].lower() # NOTE : Removed this
         utterances = list()
         for stline in range(5):
             linei = story["lines"]["{}".format(stline + 1)]
@@ -29,26 +49,21 @@ with open(_DIR + "data/commonsense/storyid_partition.txt", "r") as f:
 
             charA, charB = characters[dialog["A"]], characters[dialog["B"]]
             if(charA["app"] == True):
-                try :
-                    mystring = str(charA["emotion"]["ann0"]["plutchik"][0])
-                    re.sub('[^A-Za-z]+', '', mystring)
-                    uttr = ("A", linei["text"], [mystring[:-2]])
+                onehotm = get_labels(charA["app"])
+                if(onehotm != None):
+                    uttr = ("A", linei["text"], [onehotm])
                     utterances.append(uttr)
-                except :
-                    pass
             if(charB["app"] == True):
-                try :
-                    mystring = str(charB["emotion"]["ann0"]["plutchik"][0])
-                    re.sub('[^A-Za-z]+', '', mystring)
-                    uttr = ("B", linei["text"], [mystring[:-2]])
+                onehotm = get_labels(charB["app"])
+                if(onehotm != None):
+                    uttr = ("B", linei["text"], [onehotm])
                     utterances.append(uttr)
-                    count += 1
-                except :
-                    pass
+
         print(utterances)
         dialog["utts"] = utterances
         to_write[tdt].append(dialog)
-    to_write["train"] = to_write["valid"]
+    dev_len = len(to_write["valid"])
+    to_write["train"], to_write["valid"] = to_write["valid"][:int(0.8 * dev_len)], to_write["valid"][int(0.8 * dev_len) + 1:]
     print(count)
 
 with open(_DIR + "data/commonsense/data.pkl", "wb+") as handle:
